6bfb19922a3cd4fd1b1dea4aa853b40e6983e10b
Wes Hardaker <opensource@hardakers.net>
2011-07-22 08:31:00 -0700

    create an XML file from the txt

---
+++ AgendaToXML.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $day;
+my $time;
+
+print "<ietfschedule>\n";
+open(I, "agenda.txt");
+while(<I>) {
+    if (/^(saturday|sunday|monday|tuesday|wednesday|thursday|friday|sunday)/i) {
+	print "  </day>\n" if (defined($day));
+	$day = $1;
+	print "  <day name=\"$day\">\n";
+    }
+    if (/^(\d{4})-(\d{4})\s+(.*)/) {
+	print "    <slot>\n";
+	print "      <start>$1</start>\n";
+	print "      <end>$2</end>\n";
+	print "      <what>$3</what>\n";
+	print "    </slot>\n";
+    }
+}
+print "  </day>\n";
+print "</ietfschedule>\n";
